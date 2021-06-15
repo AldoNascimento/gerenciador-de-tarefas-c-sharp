7f010f2c2b84eaa5de8b02887b435f6831c7245c
Aldo Nascimento <aldonpaula@gmail.com>
2021-06-14 22:45:06 -0300

    Aula 2 backend C#

---
+++ GerenciadorDeTarefas/GerenciadorDeTarefas/Controllers/BaseController.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GerenciadorDeTarefas.Controllers
+{
+    public class BaseController
+    {
+    }
+}
